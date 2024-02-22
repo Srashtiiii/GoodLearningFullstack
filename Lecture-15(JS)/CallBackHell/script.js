0a2f4f969d4567f214b61b005ce90125fe1c4256
srashtiiii <srashtig03@gmail.com>
2024-02-22 22:12:12 +0530

    callbackhell

---
+++ Lecture-15(JS)/CallBackHell/script.js
@@ -0,0 +1,38 @@
+function download(cb)
+{
+    console.log("Download starts");
+    setTimeout(function()
+    {
+        console.log("Download ends");
+        cb(upload);
+    },2000);
+
+}
+
+function compress(cb)
+{
+
+    
+    console.log("Compression starts");
+    setTimeout(function()
+    {
+        console.log("Compression ends");
+        cb();
+    },2000);
+
+
+}
+
+function upload()
+{
+    
+    console.log("Upload starts");
+    setTimeout(function()
+    {
+        console.log("Upload ends");
+    },2000);
+
+}
+
+
+download(compress);
