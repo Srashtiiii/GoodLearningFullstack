b47e28626bdd9de5d27610d9a20193e04cf1290b
srashtiiii <srashtig03@gmail.com>
2024-03-07 20:06:15 +0530

    DOM

---
+++ Lecture-19(DOM)/3_EventDelegation/script.js
@@ -0,0 +1,20 @@
+
+let itemList=document.querySelectorAll('.item');
+let container=document.querySelectorAll('.container');
+
+
+
+console.log(itemList);
+
+// itemList.forEach((item)=>
+// {
+//     item.addEventListener('click',()=>{
+//         console.log(item.innerText);
+//     })
+// })
+
+container.addEventListener('click',(ev)=>{
+    console.log(ev.target);
+    let item=ev.target;
+    console.log(item.innerText);
+})
