4beff40e9593c0b42267307ac1d6a27ba7456f8e
srashtiiii <srashtig03@gmail.com>
2024-03-14 22:02:31 +0530

    arrays

---
+++ Lecture-12(JS)/1_Arrays/arrays.js
@@ -0,0 +1,15 @@
+var arr=[1,2,44,'world', true, 12.90];
+
+console.log(arr);
+
+arr.unshift('hello');
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.push('tata');
+console.log(arr);
+
+arr.pop();
+console.log(arr);
