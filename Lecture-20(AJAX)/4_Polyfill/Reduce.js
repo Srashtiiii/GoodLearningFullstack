77910afb61d6f2e2d1c22cbe815b106a8f19adec
srashtiiii <srashtig03@gmail.com>
2024-03-14 20:46:22 +0530

    reduce

--- Lecture-20(AJAX)/4_Polyfill/Reduce.js
+++ Lecture-20(AJAX)/4_Polyfill/Reduce.js
@@ -3,16 +3,28 @@ let arr=[1,2,3,4,5];
 Array.prototype.myReduce = function(fun)
 {
         let arr=this;
-        
-        for(let i=0;i<arr.length;i++)
-        {
-            
+        let acc, start_indx;
 
+        if(typeof(intiVal) != undefined) 
+        {
+            acc = intiVal;
+            start_indx = 0;
         }
+        else {
+            acc = [0];
+            start_indx = 1;
+        }
+    
+        for(let i=start_indx;i<=arr.length;i++)
+        {
+            acc=fun(acc[i],i,arr);
+        }
+        return acc;
 }
 
 let x= arr.reduce((acc,val,indx,arr)=>
 {
     return acc * val
-})
+},0)
+
 console.log(x);
