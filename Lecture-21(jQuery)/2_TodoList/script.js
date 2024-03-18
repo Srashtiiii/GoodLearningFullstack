6dfe3f4dce5accbaa4d4293f50134c6d75054a76
srashtiiii <srashtig03@gmail.com>
2024-03-18 20:41:03 +0530

    jQuery

---
+++ Lecture-21(jQuery)/2_TodoList/script.js
@@ -0,0 +1,16 @@
+)
+end (
+    $('<h1>').text('ToDo List').addClass('heading')
+
+    end (
+        ('<div')
+        .addClass('inputDetails')
+        .append(
+            $('<input>')
+            .attr('placeholder','Enter New Task')
+            .attr('type', 'text')
+            .addClass('newTask')
+        )
+
+    )
+)
