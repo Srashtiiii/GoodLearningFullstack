2b3a4564632348fd11d8585ed664916ec105a333
srashtiiii <srashtig03@gmail.com>
2024-04-11 21:01:06 +0530

    closure

--- Lecture-13(JS)/2_Closures/1_Closures.js
+++ Lecture-13(JS)/2_Closures/1_Closures.js
@@ -1,14 +1,76 @@
-function fun(){
-    console.log(x);
-    var x=10;
-    console.log(x);
+// function fun(){
+//     console.log(x);
+//     var x=10;
+//     console.log(x);
+// }
+
+// fun();
+
+// for(var i=0;i<10;i++)
+// {
+
+// }
+// console.log(i);
+// //console.log(x);
+
+
+
+
+// function parent(){
+//     var x=10;
+//     function child(){
+//         console.log(x);
+//     }
+//      return child;
+// }
+
+// let f=parent();
+
+
+
+//when parent is called again new clouser is created
+// function parent(){
+//     var x=1;
+//     function child(){
+//         x++;
+//         console.log(x);
+//     }
+//     return child;
+// }
+
+// let f=parent();
+// let f1=parent();
+// f(); //2
+// f(); //3
+// f(); //4
+
+// f1(); //2
+// f1(); //3
+// f1(); //4
+
+
+
+
+//if a bucket goes into a clourse then it will not go into another clourse
+function grandparent() {
+    var x = 1;
+    function parent() {
+        x++;
+        function child() {
+            x++;
+            console.log(x);
+        }
+        return child;
+    }
+    return parent;
 }
 
-fun();
+let fun = grandparent();
 
-for(var i=0;i<10;i++)
-{
+let f1 = fun();
+let f2 = fun();
 
-}
-console.log(i);
-//console.log(x);
+f1(); //4
+f1(); //5
+f2(); //6
+f2(); //7
