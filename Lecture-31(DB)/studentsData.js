0020f17fb8b9165d983d6d4bc3c06cf2e5e4eaa4
srashtiiii <srashtig03@gmail.com>
2024-04-17 21:40:13 +0530

    mongoDB

---
+++ Lecture-31(DB)/studentsData.js
@@ -0,0 +1,59 @@
+let students
+
+
+db.students.insertMany([
+    {
+    name:'Rajan',
+    age:19,
+    subject:'Java',
+    marks:86
+},
+{
+    name:'Rahul',
+    age:19,
+    subject:'c++',
+    marks:78
+},
+{
+    name:'Priya',
+    age:21,
+    subject:'OS',
+    marks:90
+}
+])
+
+db.students.updateMany(
+    {},
+    {
+        $set: {
+            subject: "Datascience",
+            address:"Coding Blocks"
+        }
+    }
+)
+
+db.students.updateMany(
+    {
+        marks:90
+    }
+
+)
+
+db.students.find({
+    $and:[
+        {age:22},
+        {marks:90}
+    ]
+})
+
+
+db.students.find({
+    $and:[
+        {
+            age:{
+                $gt:20
+                }
+            
+        }
+    ]
+})
