b272db03e6b53d20c75fc9a6cff121c20e79600e
srashtiiii <srashtig03@gmail.com>
2024-04-25 21:24:19 +0530

    settimeout

---
+++ Lecture-14(JS)/2_SetTimeOut/setTimeOut.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    console.log("Hello World!");
+}, 5000); // 5 seconds ka delay browser par dall diya
+//jab 5 seconds pure honge tb function ko call dega 
+
+
+
+console.log("hiii");
+console.log("byeee");
