63b8fa3b1493c439e75418915b4cc66f85510c53
srashtiiii <srashtig03@gmail.com>
2024-04-30 21:52:06 +0530

    promise

---
+++ Lecture-15(JS)/3_Promises/2_script.js
@@ -0,0 +1,22 @@
+function downloadMovie(url){
+    return new Promise(function (resolve,reject){
+        let ext=url.split('/').pop().split('.').pop();
+        if(ext != 'mp4'){
+            reject("Give correct URL");
+        }
+        else
+        {
+            setTimeout(function(){
+            resolve(url.split('/').pop());
+        },2000);
+    }
+    })
+}
+
+downloadMovie('myurl.com/avengers.mp4')
+.then(function(movie){
+    console.log(movie);
+}).catch(function(err)
+{
+    console.log(err);
+})
