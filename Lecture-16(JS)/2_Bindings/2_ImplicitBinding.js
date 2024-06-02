d77f4f224523ccdded3be2bede3781dae3a5ba19
srashtiiii <srashtig03@gmail.com>
2024-06-02 17:41:43 +0530

    js

---
+++ Lecture-16(JS)/2_Bindings/2_ImplicitBinding.js
@@ -0,0 +1,9 @@
+let obj={
+    a:1,
+    b:2,
+    fun:function(){
+        console.log(this);
+    }
+}
+
+obj.fun();
---
+++ Lecture-16(JS)/2_Bindings/3_ExplicitBinding.js
@@ -0,0 +1,22 @@
+function hello(state,country){
+    console.log("Name",this.name);
+    console.log("Hobby",this.hobby);
+    console.log("State",state);
+    console.log("country",country);
+}
+
+let person1={
+    name:"Rahul",
+    hobby:"Cricket"
+}
+
+let person2={
+    name:"Raj",
+    hobby:"Football"
+}
+
+//1. call ke through bind krna: inbuilt function to change the bind inside hello function
+//SYNTAX: hello.call(this, arguments);
+//hello.call(person1); //hello ke andar jo this hoga that will point to 'person1'
+hello.call(person2,"Delhi","India"); 
+g
