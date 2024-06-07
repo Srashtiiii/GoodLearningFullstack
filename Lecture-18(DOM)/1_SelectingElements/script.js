56b159ee8059b6b3705a61505949f5cd01ec7789
srashtiiii <srashtig03@gmail.com>
2024-06-07 17:43:56 +0530

    DOM

--- Lecture-18(DOM)/1_SelectingElements/script.js
+++ Lecture-18(DOM)/1_SelectingElements/script.js
@@ -2,3 +2,6 @@ let divs=document.getElementsByTagName('div');
 console.log(divs);
 
 let movies = document.getElementsByClassName('movie');
+console.log(movie);
+
+const avengers=document.getElementById('avengers');
