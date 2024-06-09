dc1b21344143088801a4144aa2b932635725648f
srashtiiii <srashtig03@gmail.com>
2024-06-09 17:43:40 +0530

    DOM

--- Lecture-19(DOM)/5_Debouncing/script.js
+++ Lecture-19(DOM)/5_Debouncing/script.js
@@ -1,13 +1,22 @@
-let body=document.querySelector('body');
-let navBar=document.querySelector('navBar');
+let body = document.querySelector('body');
+let navBar = document.querySelector('.navBar');
 
-window.onscroll=(ev)=>{
-    // console.log(window.scrollY);
-    if(window.scrollY>300) {  
-        navBar.style.backgroundColor="orange";
-      }
-      else{
-        navBar.style.backgroundColor="white";
-      }
-      
+function scrollKaFunction() {
+    console.log("Scrolling")
+}
+
+window.addEventListener('scroll', debounce(scrollKaFunction, 1000));
+
+// window.addEventListener('scroll',function)
+
+function debounce(fun, delay) {
+    let id;
+    return function () {
+        console.log("Clearing Interval",id);
+        clearTimeout(id);
+        id = setTimeout(()=>{
+            fun();
+        },delay);
+        console.log("New Interval id",id);
+    }
 }
