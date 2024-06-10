581d048dd222aa929952f682939d884227ea2d0c
srashtiiii <srashtig03@gmail.com>
2024-06-10 21:56:16 +0530

    AJAX

--- Lecture-20(AJAX)/4_Polyfill/Filter.js
+++ Lecture-20(AJAX)/4_Polyfill/Filter.js
@@ -1,23 +1,21 @@
-Array.prototype.myFilter=function(fun){
-    let arr=this;
-    let newArr=[];
-    for(let i=0 ; i<arr.length ; i++)
-    {
-        const element=arr[i];
-        let addToArray=fun(arr[i],i,arr);
-        addToArray?newArr.push(arr[i]): "";
+let arr = [1,2,3,34,4,5];
+
+Array.prototype.myFilter = function(fun){
+    let arr = this;
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let addToArray = fun(arr[i],i,arr) ;
+        addToArray ? newArr.push(arr[i]) : "";
     }
-    return newArr
+    return newArr;
 }
 
 
-let arr=[1,2,3, 34,4,5];
-let newArr=arr.myFilter((val,index,arr)=>{
+let newArr = arr.myFilter((val,indx,arr)=>{
     if(val%2 == 0) return true;
     return false;
 })
-
-
 
 
 console.log(newArr);
--- Lecture-20(AJAX)/4_Polyfill/Reduce.js
+++ Lecture-20(AJAX)/4_Polyfill/Reduce.js
@@ -1,30 +1,26 @@
-let arr=[1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 
-Array.prototype.myReduce = function(fun, initVal)
-{
-        let arr=this;
-        let acc, start_indx;
+Array.prototype.myReduce = function (fun, initVal) {
+    let arr = this;
+    let acc, start_indx;
 
-        if(typeof(intiVal) != "undefined") 
-        {
-            acc = intiVal;
-            start_indx = 0;
-        }
-        else {
-            acc = [0];
-            start_indx = 1;
-        }
-    
-        for(let i=start_indx;i<=arr.length;i++)
-        {
-            acc=fun(acc[i],i,arr);
-        }
-        return acc;
+    if (typeof(initVal) != "undefined") {
+        acc = initVal;
+        start_indx = 0;
+    }
+    else {
+        acc = arr[0];
+        start_indx = 1;
+    }
+
+    for (let i = start_indx; i < arr.length; i++) {
+        acc = fun(acc, arr[i], i, arr);
+    }
+    return acc;
 }
 
-let x= arr.myReduce((acc,val,indx,arr)=>
-{
+let x = arr.myReduce((acc, val, indx, arr) => {
     return acc * val
-})
+},0)
 
 console.log(x);
