10ab1fdfb6278d027c5c081b82f0bc200a732900
srashtiiii <srashtig03@gmail.com>
2024-06-15 22:32:00 +0530

    jQuery

--- Lecture-21(jQuery)/2_TodoList/script.js
+++ Lecture-21(jQuery)/2_TodoList/script.js
@@ -1,16 +1,23 @@
-)
-end(
-    $('<h1>').text('ToDo List').addClass('heading')
-
-    end(
-        ('<div')
-            .addClass('inputDetails')
-            .append(
-                $('<input>')
-                    .attr('placeholder', 'Enter New Task')
-                    .attr('type', 'text')
-                    .addClass('newTask')
-            )
-
+$('body')
+    .append(
+        $('<h1>').text('ToDo List').addClass('heading')
     )
-)
+
+    .append(
+        $('<input>')
+            .attr('placeholder', 'Enter New Task')
+            .attr('type', 'text')
+            .addClass('newTask')
+    )
+
+
+    .append(
+        $('<button>')
+            .text('Add Task')
+            .addClass('btn')
+    )
+
+    .append(
+        $('<ul>')
+            .addClass('taskList')
+    )
