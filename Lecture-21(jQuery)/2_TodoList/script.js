e1933aa65d74ad604df0e6189fdeb93baeca683c
srashtiiii <srashtig03@gmail.com>
2024-06-20 20:30:25 +0530

    TodoList

--- Lecture-21(jQuery)/2_TodoList/script.js
+++ Lecture-21(jQuery)/2_TodoList/script.js
@@ -1,23 +1,63 @@
 $('body')
     .append(
-        $('<h1>').text('ToDo List').addClass('heading')
+        $('<h1>').text('Todo List').addClass('heading')
     )
-
     .append(
-        $('<input>')
-            .attr('placeholder', 'Enter New Task')
-            .attr('type', 'text')
-            .addClass('newTask')
+        $('<div>')
+            .addClass('inputDetails')
+            .append(
+                $('<input>')
+                    .attr('placeholder', 'Enter New Task')
+                    .attr('type', 'text')
+                    .addClass('newtask')
+            )
+            .append(
+                $('<button>')
+                    .text('Add Task')
+                    .addClass('btn')
+                    .click(ev => {
+                        if ($('.newtask').val().trim() != '') {
+                            $('.tasklist')
+                                .append(
+                                    $('<li>')
+                                        .addClass('task')
+                                        .text(
+                                            $('.newtask').val()
+                                        )
+                                        .append(
+                                            $('<div>')
+                                                .addClass('btngrp')
+                                                .append(
+                                                    $('<button>').text('↑').addClass('upBtn')
+                                                )
+                                                .append(
+                                                    $('<button>').text('↓').addClass('downBtn')
+                                                )
+                                                .append(
+                                                    $('<button>').text('❌').addClass('deleteBtn')
+                                                )
+                                        )
+                                )
+                            $('.newtask').val('')
+                        }
+
+                    })
+            )
     )
-
-
-    .append(
-        $('<button>')
-            .text('Add Task')
-            .addClass('btn')
-    )
-
     .append(
         $('<ul>')
-            .addClass('taskList')
+            .addClass('tasklist')
+            .click(ev => {
+                if ($(ev.target).hasClass('upBtn')) {
+                    $(ev.target).parent().parent()
+                        .insertBefore($(ev.target).parent().parent().prev())
+                }
+                else if ($(ev.target).hasClass('downBtn')) {
+                    $(ev.target).parent().parent()
+                        .insertAfter($(ev.target).parent().parent().next())
+                }
+                else if ($(ev.target).hasClass('deleteBtn')) {
+                    $(ev.target).parent().parent().remove()
+                }
+            })
     )
