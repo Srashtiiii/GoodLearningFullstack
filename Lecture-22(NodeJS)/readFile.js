c8f203de78bfd660377c71d8e8fc73e31654aaa9
srashtiiii <srashtig03@gmail.com>
2024-06-21 15:12:46 +0530

    NodeJS

--- Lecture-22(NodeJS)/readFile.js
+++ Lecture-22(NodeJS)/readFile.js
@@ -1,11 +1,15 @@
-const fs=require('fs/promises');
+const fs = require('fs/promises');
 
-let filepath= __dirname + 'database/data.json';
+let filepath = __dirname + '/data.json'
+// console.log(filepath);
 
-fs.readFile(filepath)
-  .then(data) => {
-        console.log(data.toString());
-           })  
-           .catch(err => {
-                console.log(err);
-           )};
+fs.readFile(filepath,{
+    encoding: 'utf-8'
+}).then((data)=>{
+    data = JSON.parse(data);
+    console.log(data.text);
+    console.log(data.date);
+})
+.catch((err)=>{
+    console.log(err);
+})
--- Lecture-22(NodeJS)/writeFile.js
+++ Lecture-22(NodeJS)/writeFile.js
@@ -1,13 +1,12 @@
 //to use fs module we will use the require it in our file.
 const fs=require('fs');
-
-
+ 
 //filepath: path absolute hota h..meaning computer ki root 
 //directoty se laker jis bhi file me data likhna h us file tak ka path
 // fs.writeFile(filePath)
 
 
-// console.log(__dirname); // willl get path of the current file we are working in.
+// console.log(__dirname); // will get path of the current file we are working in.
 
 let filepath=__dirname + '/data.txt'
 console.log(filepath);
