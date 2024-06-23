19509ae4792866a0d9b6591a88af028e4901edfc
srashtiiii <srashtig03@gmail.com>
2024-06-23 13:30:02 +0530

    express

--- Lecture-24/app.js
+++ Lecture-24/app.js
@@ -7,4 +7,33 @@ app.get('/',(req, res)=>{
     res.send("Hello");
 })
 
+app.get('/greeting',(req, res)=>{
+    res.send("Good Evening,sir");
+})
+
+
+app.get('/greeting',(req, res)=>{
+    res.send("<h1>Good Evening,sir</h1>");
+})
+
+//PARAMS req: /greet/srashti' 
+
+app.get('/greet/:name',(req,res)=>{
+    console.log(req.params);
+    res.send(`Hello ${req.params.name}`);
+
+})
+
+
+
+//QUERY PARAMETERS req: '/bye?=name=srashti&city=mathura
+
+
+
+app.get('/bye',(req,res)=>{
+    res.send('Hello ${req.query.name} from ${req.query.city}');
+})
+
+
 app.listen(PORT);
+console.log('http://localhost:${PORT}');
