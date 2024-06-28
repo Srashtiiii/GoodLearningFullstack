27252bc4dee511fa220d4e371c6ed47fb8ee185e
srashtiiii <srashtig03@gmail.com>
2024-06-28 15:06:48 +0530

    todolist

--- Lecture-25/TodoList/app.js
+++ Lecture-25/TodoList/app.js
@@ -5,7 +5,18 @@ const PORT = 4444;
 
 app.use(express.static(path.join(__dirname, "public")));        
 
-let todo = ["Dancing", " Singing", "Coding", "Reading"];
+let todos = ["Dancing", " Singing", "Coding", "Reading"];
+
+app.get('/todos', (req, res) => {
+    res.send(todos);
+})
+
+
+app.get('/addtasks', (req,res) => {
+    let {task}=req.query;
+    todos.push(task);
+    res.redirect('/todos');
+});
 
 
 app.listen(PORT,()=>{
---
+++ Lecture-25/TodoList/public/script.js
@@ -0,0 +1,9 @@
+const btn=document.querySelector('.btn');
+const inp=document.querySelector('.inp');
+
+btn.addEventListener('click',(ev)=>{
+    ev.preventDefault();
+    try{
+        let {data}=
+    }
+})
